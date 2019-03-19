de9c5cc23f8a44a7096d0edb2876bebb0ae78976
Sandeep Kumar Mandal <sandeep.sandeep.mandal@gmail.com>
2019-03-19 21:13:01 +0530

    Filters

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+var app = angular.module('mainApp', []);
+// this runs before the app is loaded
+app.config(function() {
+
+});
+// this runs while the app is running
+app.run(function() {
+
+});
+// controller thats controls the data for the application/ section based on type
+app.controller('mainController', ['$scope', function($scope) {
+    $scope.name = 'Sandeep';
+    $scope.genderArray = ['male', 'female', 'others'];
+}]);
